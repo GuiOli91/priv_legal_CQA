be9fec3ef3191dfc01c4594fec2340b36da197e6
Guilherme Oliveira <guilherme.oliveira.mec@gmail.com>
2024-01-09 09:15:15 +0100

    webscrap files and environment

--- webscrap2.py
+++ webscrap2.py
@@ -3,12 +3,17 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import (
+    NoSuchElementException
+)
 
 import os
 import json
+import re
 
 OUTPUT_DIR = "web_out"
 FILE = "data.json"
+OUT = "qa.json"
 
 driver = webdriver.Remote("http://localhost:4444", options=webdriver.ChromeOptions())
 
@@ -19,11 +24,16 @@ data_dict = json.loads(json_data)
 
 wait = WebDriverWait(driver, 10)
 
+q_as = {}
+
 for page, links in data_dict.items():
   i = 0
+  q_as[f"{page}"] = {}
   for url in links:
+    
     try:
-      
+      q_as[f"{page}"][f"{str(i)}"] = {}
+      q_as[f"{page}"][f"{str(i)}"]["question"] = {}
       # Navigate to the question url
       driver.get(url)
       
@@ -32,93 +42,76 @@ for page, links in data_dict.items():
       topic_tags = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "content-topic-tags")))
       
       topic_tags = topic_tags.text.split("\n")
+      q_as[f"{page}"][f"{str(i)}"]["question"]['topic_tags'] = topic_tags
       
-      content_question = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "question")))
+      content_question = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "content-question")))
     
-      
+      q_title = content_question.find_element(By.TAG_NAME, "h1").text
+      q_as[f"{page}"][f"{str(i)}"]["question"]['title'] = q_title
+      q_body = content_question.find_element(By.ID, "question-body").text
+      q_as[f"{page}"][f"{str(i)}"]["question"]['body'] = q_body
     
-      i = i + 1
+      header_details = content_question.find_element(By.CLASS_NAME, "header-details")
       
+      location = header_details.find_element(By.TAG_NAME, "span").text
+      q_as[f"{page}"][f"{str(i)}"]["question"]['location'] = location
+      date = header_details.find_element(By.TAG_NAME, "time").accessible_name
+      q_as[f"{page}"][f"{str(i)}"]["question"]['date'] = date
+      answers = header_details.find_element(By.CLASS_NAME, "answers-count").text
+      q_as[f"{page}"][f"{str(i)}"]["question"]['answers'] = answers
+      
+      answers = re.findall(r'\d+', answers)[0]
+    
     except Exception as e:
       # Handle any exceptions that may occur
       print(f"An error occurred: {e}")
       continue
       
+    if int(answers) > 0:
+      q_as[f"{page}"][f"{str(i)}"]["answers"] = {}
+      
+      answers = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "qa-answer")))
+      
+      for answer in answers:
+        try:
+          q_as[f"{page}"][f"{str(i)}"]["answers"][f"{answers.index(answer)}"] = {}
+          
+          lawyer = answer.find_element(By.CLASS_NAME, "lawyer-info")
+          lawyer_name = lawyer.find_element(By.TAG_NAME, "h3").text
+          q_as[f"{page}"][f"{str(i)}"]["answers"][f"{answers.index(answer)}"]['lawyer_name'] = lawyer_name
+          
+          answer_body = answer.find_element(By.CLASS_NAME, "answer-body").text
+          q_as[f"{page}"][f"{str(i)}"][
+            "answers"][f"{answers.index(answer)}"]['answer_text'] = answer_body
+          
+          cta_answer = answer.find_element(By.CLASS_NAME, "cta-qa-content")
+          help_count = cta_answer.find_element(By.CLASS_NAME, "answer-upvote-section").text
+          q_as[f"{page}"][f"{str(i)}"][
+            "answers"][f"{answers.index(answer)}"]['help_count'] = help_count
+          
+          agree_lawyers = cta_answer.find_element(By.CLASS_NAME, "agrees-lawyers").text
+          q_as[f"{page}"][f"{str(i)}"][
+            "answers"][f"{answers.index(answer)}"]['agree_lawyers'] = agree_lawyers
+          
+        except NoSuchElementException:
+          continue
+        except Exception as e:
+          print(f"An error occurred: {e}")
+          break
+    
+    i = i + 1
+      
 driver.quit()
         
-      
         
+folder_path = os.path.join(os.getcwd(), OUTPUT_DIR)
 
-URL = "https://www.avvo.com"
-PATH = "/topics/bankruptcy/advice"
-# URL = "https://www.avvo.com/topics/bankruptcy/advice?utf8=✓&search_topic_advice_search[state]=CA&search_topic_advice_search[content_type]=question&ajax=1"
+if not os.path.exists(folder_path):
+  os.makedirs(folder_path)
 
-PARAMS = {'utf8':'✓',
-          'search_topic_advice_search[state]':'CA',
-          'search_topic_advice_search[content_type]':'question',
-          'ajax':'1',
-          }
+json_data = json.dumps(q_as)
 
-url_parms = f"{URL}{PATH}?{'&'.join([f'{k}={v}' for k,v in PARAMS.items()])}"
-
-# options = webdriver.FirefoxOptions()
-# driver = webdriver.Firefox(options=options)
-
-driver = webdriver.Remote("http://localhost:4444", options=webdriver.ChromeOptions())
-
-try:
-    # Set a timeout for waiting for elements to load (e.g., 10 seconds)
-    wait = WebDriverWait(driver, 10)
-
-    # Navigate to the URL
-    driver.get(url_parms)
-    
-    # Get the Maximum page number
-    pages = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "pagination-page-v2")))
-    page = pages[-1].text
-    page = int(page)
-    
-    links = {}
-    for i in range(page):
-        if i != 0:
-            PARAMS['page'] = i + 1
-            url_parms = f"{URL}{PATH}?{'&'.join([f'{k}={v}' for k,v in PARAMS.items()])}"
-            driver.get(url_parms)
-
-        # Wait until all the elements with class "topic-advice-question-card" are present
-        topic_questions = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "topic-advice-question-card")))
-
-        # Loop through each "topic-question" element
-        questions = []
-        for topic_question in topic_questions:
-            # Find all elements with class "block-link" within the current "topic-question" element
-            block_link = topic_question.find_element(By.CLASS_NAME, "block-link")
-
-            # Print the text content of each "block-link" element
-            href = block_link.get_attribute("href")
-            
-            if href:
-                questions.append(href)
-                
-        links[f"page_{i+1}"] = questions
-        
-    folder_path = os.path.join(os.getcwd(), OUTPUT_DIR)
-
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    json_data = json.dumps(links)
-
-    with open(os.path.join(folder_path, "data.json"), "w") as file:
-        file.write(json_data)
-    
-
-except Exception as e:
-    # Handle any exceptions that may occur
-    print(f"An error occurred: {e}")
-
-finally:
-    # Close the browser window
-    driver.quit()
+with open(os.path.join(folder_path, OUT), "w") as file:
+      file.write(json_data)
 
 
